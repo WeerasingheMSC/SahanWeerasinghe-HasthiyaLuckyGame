419f21df11adc8f8d79886ab4824aa6005ab36fc
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2025-12-19 11:06:43 +0530

    Basic Application Structure Frontend+Backend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className=' text-center font-bold text-5xl mt-5'> Lucky 4 </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
